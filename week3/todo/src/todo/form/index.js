c589310c0ef5f6f6c2d470fca5debf514601e5bb
relkimm <bbonggu92@gmail.com>
2023-02-05 15:57:27 +0900

    week3: todo 추가 기능 구현

--- week3/todo/src/todo/form/index.js
+++ week3/todo/src/todo/form/index.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { addTodoAction } from "../store/action";
 import AddButton from "./components/AddButton";
 import TodoInput from "./components/TodoInput";
 import "./style.css";
 
 export default function TodoForm() {
+  const dispatch = useDispatch();
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const todoText = event.target["todo"].value;
+    dispatch(addTodoAction(todoText));
+    event.target["todo"].value = "";
+  };
+
   return (
-    <form className="todoForm">
+    <form className="todoForm" onSubmit={onSubmit}>
       <TodoInput />
       <AddButton />
     </form>
