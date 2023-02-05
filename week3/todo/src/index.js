147d9f0cd20621e064bd36af29826a17c8637620
relkimm <bbonggu92@gmail.com>
2023-02-05 15:00:48 +0900

    week3: redux & react-redux 설정 추가

--- week3/todo/src/index.js
+++ week3/todo/src/index.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { combineReducers, legacy_createStore } from "redux";
+import { Provider } from "react-redux";
+import todoReducer from "./todo/store/reducer";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootReducer = combineReducers({
+  todo: todoReducer,
+});
+const store = legacy_createStore(rootReducer);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
